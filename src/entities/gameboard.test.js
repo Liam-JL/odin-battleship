d779dfe8db7acb74d66260f097f9f51c67bd9bdc
LiamJL <paledeepened@gmail.com>
2025-06-16 16:42:02 +0100

    Implement tests for _isValidPlacement() method

--- src/entities/gameboard.test.js
+++ src/entities/gameboard.test.js
@@ -37,4 +37,33 @@ describe("_getShipCells()", () => {
     })
 
 })
+
+describe("_isValidPlacement()", () => {
+    test("valid cells return true", () => {
+        const board = new Gameboard();
+        expect(board._isValidPlacement([[0,0],[1,0],[2,0]])).toBe(true);
+    })
+
+    test("Invalid rows return false", () => {
+        const board = new Gameboard();
+        expect(board._isValidPlacement([[10,0],[11,0],[12,0]])).toBe(false);
+    })
+
+    test("column off the board returns false", () => {
+        const board = new Gameboard();
+        expect(board._isValidPlacement([[2,-2],[2,-1],[2,0],[2,1],[2,2]]))
+    })
+
+    test("_isValidPlacement() returns true for _getShipCells() when valid input is given", () => {
+        const board = new Gameboard();
+        const goodPlacement = board._getShipCells(5, [4,3], "horizontal");
+        expect(board._isValidPlacement(goodPlacement)).toBe(true);
+    })
+
+    test("_isValidPlacement() returns false for _getShipCells() when invalid input is given", () => {
+            const board = new Gameboard();
+            const badPlacement = board._getShipCells(5, [6,6], "horizontal");
+            expect(board._isValidPlacement(badPlacement)).toBe(false);
+    })
+})
  
