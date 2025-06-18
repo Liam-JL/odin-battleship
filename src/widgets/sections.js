45b97ec4ad747f77169e75729c2f9939a1ab929e
LiamJL <paledeepened@gmail.com>
2025-06-18 16:24:45 +0100

    Implement initial styling for app and player and computer sections

--- src/widgets/sections.js
+++ src/widgets/sections.js
@@ -1,8 +1,8 @@
 import { Player } from "../entities/player";
 import { generateBoard } from "../features/generate-board";
 
+
 export function playerSection() {
-    const player = new Player();
     const section = document.createElement("section");
     section.classList.add("player-section");
     section.innerHTML = `
@@ -22,7 +22,7 @@ export function computerSection() {
     const section = document.createElement("section");
     section.classList.add("player-section");
     section.innerHTML = `
-        <div class="gameboard"></div>
+        <div class="gameboard gameboard--computer"></div>
         <span>Computer grid</span>
     `;
     
