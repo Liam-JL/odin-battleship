a3931470eef1af587c2852b883c8d5555355ef9c
LiamJL <paledeepened@gmail.com>
2025-07-05 16:46:20 +0100

    Update README

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import "./shared/styles/app.css"
 import "./shared/styles/widgets.css"
-import "./shared/images/battleship-ss.png"
 import { renderAppWidgets } from "./pages/render_app_widgets";
 window.onload = (e) => {
     renderAppWidgets();
